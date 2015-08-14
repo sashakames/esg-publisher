b20900985efec5155e9948f1d67c6a7bce3c9d4a
Sasha Ames <ames4@tux352.llnl.gov>
2015-08-14 10:17:36 -0700

    first cut

---
+++ src/python/esgcet/esgcet/config/xmlconfig.py
@@ -0,0 +1,50 @@
+from lxml import etree
+
+def get_fvalue(ele):
+
+    chd = ele.getchildren()
+
+
+    if len(chd) == 0:
+        return [[]]
+
+#        res =  ele.get("value")
+
+#        if not res is None:
+#            return [[res.split(',')]]
+#        else:
+#            return []
+
+    arr = []
+
+    for x in chd:
+ 
+        vals = x.get("value")
+       
+        res = get_fvalue(x)
+
+        valarr = []
+
+        if not vals is None:
+            valarr = [vals.split(',')]
+
+        for y in res:
+            arr.append( valarr + y)
+    return arr
+
+    
+
+def get_config(fn):
+
+    doc = etree.parse(fn)
+
+    root = doc.getroot()
+
+    fields = get_fvalue(root)
+
+    return fields
+
+
+import sys
+
+print str(get_config(sys.argv[1]))
