589744ef21dc876ef0703b9ae86ff491190a7e92
sashakames <ames4@llnl.gov>
2020-09-22 09:41:46 -0700

    add mkd call

--- pkg/autocwrap.py
+++ pkg/autocwrap.py
@@ -12,12 +12,14 @@ for line in open(maplist):
     map_json_data = mp.run([fullmap, 'no'])
     datafile=map_json_data[0][1]
 
-    destpath=os.path.dirname()
-    outname=os.path.basename()
-    idx = outname.rfind(.)
+    destpath=os.path.dirname(datafile)
+    outname=os.path.basename(datafile)
+    idx = outname.rfind('.')
     scanfntemplate = "{}.scan.json"
     scanpath=scanfntemplate.format(outname[0:idx])
 
     autstr='autocurator --out_pretty --out_json {} --files "{}/*.nc"'
     os.system(autstr.format(scanpath, destpath))
-    mk_dataset
+    
+    mkd.run([map_json_data, scanpath, "greyworm1-rh7.llnl.gov", "esgf-fedtest.llnl.gov", False, 'no'])
+
