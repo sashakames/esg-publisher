0aa9712ece0d2c7acfbfa0f82db42f5ddcd534e8
sashakames <ames4@llnl.gov>
2024-10-31 20:32:53 +0000

    update search query client code

--- src/python/esgcet/globus_query.py
+++ src/python/esgcet/globus_query.py
@@ -4,6 +4,9 @@ import sys, os
 
 from subprocess import Popen, PIPE
 
+from globus_sdk import SearchClient
+
+
 FILTER_TEMPLATE = {
     "type" : "match_all",
     "field_name" : "data_node",
@@ -28,6 +31,7 @@ class ESGGlobusQuery():
     def __init__(self, UUID_in, data_node_in):
         self._UUID = UUID_in
         self._data_node_filter = self._add_filter("data_node", data_node_in)
+        self._search_client = SearchClient()
 
     def _add_filter(self, name, value):
         tmpfilter = copy.deepcopy(FILTER_TEMPLATE)
@@ -40,18 +44,11 @@ class ESGGlobusQuery():
         return tmpfilter
     
     def globus_get_record(self, subj):
-        print(f"DEBUG {subj}")
-        proc = Popen([GLOBUS_CMD, "search", "subject", "show", self._UUID, subj[0].rstrip()], stdout=PIPE)
-#        cmdstr = f"globus search subject show {self._UUID} '{subj.strip()}'"
-#        proc = Popen(cmdstr            , shell=True, stdout=PIPE)
- #       print(f"DEBUG {cmdstr}")
+#        print(f"DEBUG {subj}")
 
- #       os.system(cmdstr)
-        proc.wait()
-#        res = proc.stdout.read().decode()
-#        print(f"DEBUG {res}")
-        return json.load(proc.stdout)
-
+        res = self._search_client.get_subject(self._UUID, subj)
+        return res
+        
     def query_file_records(self, dataset_id, post_proc=True, latest=True, crit={}):
         q = copy.deepcopy(SEARCH_TEMPLATE)
         q["filters"].append(self._add_filter("type", "File"))
@@ -66,15 +63,8 @@ class ESGGlobusQuery():
         if latest:
             q["filters"].append(self._add_filter("latest", "true"))
         res = self._run_query(q, False)
-        print(f"DEBUG {res}")
-        if post_proc:
-            y = []
-            for x in res:
-                y.append(self._post_proc_query([x]))
-            return y
-        else:
-            return res
-
+        return [it["entries"][0]["content"] for it in res["gmeta"]]
+        
     def dataset_query_master(self, master_id):
         self._dataset_query(master_id, "master_id")
     
@@ -92,27 +82,9 @@ class ESGGlobusQuery():
         return self._post_proc_query(res)
 
     def _run_query(self, q, single=False):
-        temp = tempfile.NamedTemporaryFile(mode="w", delete=False)
-        json.dump(q, temp, indent=1)
-        print(f"DEBUG: to {temp.name}")
-        temp.close()
 
-        subproc = Popen([GLOBUS_CMD, "search", "query", self._UUID, "--query-document",temp.name], stdout=PIPE)
-
-        subproc.wait()
-        
-        if single:
-            subj = ""
-            for x in subproc.stdout:
-                if not subj:
-                    raise RuntimeError(f"non empty {subj}")
-                # There should be only one latest, TODO convert to raise an Exception
-                subj = x.decode().rstrip()
-        else:
-            subj = []
-            for x in subproc.stdout:
-                subj.append(x.decode().rstrip())
-  
+        subj = self._search_client.post_search(self._UUID, q)
+          
         return subj
 
     def _post_proc_query(self, subj): 
